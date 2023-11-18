4329e18187a46e3e72a25cb40adfd7f6af718e06
Giordano Rossiello <giordano.rossiello@gmail.com>
2023-11-18 16:17:44 +0100

    Even more advanced

---
+++ iterator/iterator.py
@@ -0,0 +1,17 @@
+class Iterator:
+
+    def __init__(self, limit):
+        self.limit = limit
+
+    def __iter__(self):
+        self.x = 10
+        return self
+
+    def __next__(self):
+        x = self.x
+
+        if x > self.limit:
+            raise StopIteration
+
+        self.x = x + 1
+        return x
